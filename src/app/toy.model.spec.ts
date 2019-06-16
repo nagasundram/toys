a086f18dba9bd857e611124e6067dc5d3b1f2393
nagasundram <nagasundram@gmail.com>
2019-06-16 12:30:58 +0530

    Toys-1.0.0

--- src/app/toy.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Toy } from './toy.model';
-
-describe('Toy', () => {
-  it('should create an instance', () => {
-    expect(new Toy()).toBeTruthy();
-  });
-});
--- src/app/toy.model.ts
+++
@@ -1,7 +0,0 @@
-export class Toy {
-  id?: string;
-  name: string;
-  movie: string;
-  imageData: string;
-  created_at: Date;
-}
